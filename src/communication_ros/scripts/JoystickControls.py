d78853a14eb622f29f978b52ae1a5ce65e8a5245
Kyle Wuerch <kwuerch@kyle98678@gmail.com>
2016-04-09 22:06:50 -0700

    Added Sean's Updated JoysticControls with Cruise Control

--- src/communication_ros/scripts/JoystickControls.py
+++ src/communication_ros/scripts/JoystickControls.py
@@ -4,7 +4,7 @@ import rospy
 import pygame
 import sys
 from std_msgs.msg import UInt8, UInt16
-
+	
 def KeyboardCtrls():
 	#initialize the publishers
 	pubFNR = rospy.Publisher("Set_FNR", UInt8, queue_size=1000)#100000
@@ -14,31 +14,8 @@ def KeyboardCtrls():
 	
 	#initialize pygame & joystick
 	pygame.init()
-	size = width,height = 420,320
-	screen = pygame.display.set_mode(size)
-	color = (200, 200, 200)
-	black = (0, 0, 0)
-	pygame.display.set_caption("HUD")
-	screen.fill(color)
-
-	font = pygame.font.SysFont('Arial', 20)
-	#screen.blit(font.render('Hello!', True, (255, 0, 0)))
-	screen.blit(font.render('Joystick Summary', True, (255,0,0)), (10, 10))
-	screen.blit(font.render('FNR Status: ', True, (255,0,0)), (10, 40))
-	screen.blit(font.render('Throttle Status: ', True, (255,0,0)), (10, 70))
-	screen.blit(font.render('Steering Status: ', True, (255,0,0)), (10, 100))
-	
-	FNR_print = "zero"
-	throttle_print = "zero"
-	steering_print = "zero"	
-
-
-	pygame.display.update()
-
 	controller = pygame.joystick.Joystick(0)
 	controller.init()
-
-	#if bored, more gui/ game stuff
 	
 	#set the message values
 	throttle = 0.0
@@ -49,113 +26,107 @@ def KeyboardCtrls():
 	max_throttle = 40.0
 	max_steering = 65535
 
-	#rate multipliers for throttle & steering
-	throttle_const = 200.0
-	steering_const = 2.0
-	#rate rates for throttle & steering
-	throttle_delta = sys.maxint/(max_throttle * throttle_const)
-	steering_delta = sys.maxint/(steering_const * max_steering)
-	rate = rospy.Rate(30) #3600
-
 	#if no new value = don't send new msg
-	old_throttle = 0
 	old_publishedThrottle = 0
-	old_steering = 0
 	old_publishedSteering = 0
 	old_FNR = 0
 
+	fastMode = True
+	cruiseControl = False
 	while not rospy.is_shutdown():
-		buttonPressed = None
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
 			if event.type == pygame.JOYBUTTONDOWN:
-				if not controller.get_button(2) == 0:
-					buttonPressed = 'X'
-				if not controller.get_button(0) == 0:
-					buttonPressed = 'A'
-				if not controller.get_button(1) == 0:
-					buttonPressed = 'B'
-				if not controller.get_button(3) == 0:
-					buttonPressed = 'Y'
+				#Foward and background ctrls(right joystick up and down is forward & backward)
+				if not controller.get_button(2) == 0: #Button X
+					old_FNR = FNR
+					if cruiseControl:
+						cruiseControl = not cruiseControl
+					FNR = 0
+				if not controller.get_button(0) == 0: #Button A
+					old_FNR = FNR
+					if cruiseControl:
+						cruiseControl = not cruiseControl
+					FNR = 2
+				if not controller.get_button(1) == 0: #Button X
+					old_FNR = FNR
+					if cruiseControl:
+						cruiseControl = not cruiseControl
+					FNR = 0
+				if not controller.get_button(3) == 0: #Button Y
+					old_FNR = FNR
+					if cruiseControl:
+						cruiseControl = not cruiseControl
+					FNR = 1
+					
+				if not controller.get_button(5) == 0: #Right Button
+					cruiseControl = not cruiseControl
+				if not controller.get_button(7) == 0: #Right Trigger
+					fastMode = False
+				if not controller.get_button(8) == 0: #Left Trigger
+					fastMode = False
+		# Steering ctrls slowMode (right trigger to turn right, left trigger to turn left)
+		leftSteering = controller.get_axis(2)
+		if leftSteering > 0.05:
+			fastMode = False
+			if steering > -32765:
+				steering -= 0.15
+		"""
+		elif leftSteering < -0.05 and abs(1 - leftSteering) > 0.05:
+			fastMode = False
+			if steering > -32765:
+				steering -= 0.05
+		"""
+			
+		rightSteering = controller.get_axis(5)
+		if rightSteering > 0.05:
+			fastMode = False
+			if steering < 32765:
+				steering += 0.15
+		"""
+		elif rightSteering < -0.05 and abs(1 - rightSteering) > 0.05:
+			fastMode = False
+			if steering < 32765:
+				steering += 0.05
+		"""
+		#Steering ctrls Continued (right joystick left to turn left, right to turn right)
+		if controller.get_axis(3) < -1*0.10:
+			fastMode = True
+			if fastMode == True:
+				tiltValue = round(controller.get_axis(3), 1)
+				steering = 0.5*max_steering* tiltValue
+		elif controller.get_axis(3) > 0.10:
+			fastMode = True
+			if fastMode == True:
+				tiltValue = round(controller.get_axis(3), 1)
+				steering = 0.5*max_steering* tiltValue
+		elif fastMode == True:
+			steering = 0
+
 		#Throttle ctrls(left joystick up incr throttle, down decr throttle)
-		if controller.get_axis(1) < -1*0.05:	
-			old_throttle = throttle
+		if controller.get_axis(1) < -1*0.05 and cruiseControl == False:	
 			tiltValue = round(-1*controller.get_axis(1), 1)
 			throttle = 40.0 * tiltValue
-		else:
+		elif cruiseControl == False:
 			throttle = 0
-
-		#Steering ctrls(right joystick left to turn left, right to turn right)
-		if controller.get_axis(3) < -1*0.10:
-			old_steering = steering
-			tiltValue = round(controller.get_axis(3), 1)
-			steering = 0.5*max_steering* tiltValue
-		elif controller.get_axis(3) > 0.10:
-			old_steering = steering
-			tiltValue = round(controller.get_axis(3), 1)
-			steering = 0.5*max_steering* tiltValue
-		else:
-			steering = 0
-				
-		#Foward and background ctrls(right joystick up and down is forward & backward)
-		# FNR Values: 0 = forward, 1 = neutral, 2 = reverse
-		if buttonPressed == 'Y':
-			old_FNR = FNR
-			FNR = 0
-			FNR_print = "0"
-			#rect = pygame.draw.rect(screen, (black), (175, 75, 200, 100), 2)
-			screen.fill(color)
-
-			screen.blit(font.render(FNR_print, True, (255,0,0)), (200, 40))
-			pygame.display.update()
-
-
-		elif buttonPressed == 'A':
-			old_FNR = FNR
-			FNR = 2
-			FNR_print = "2"
-			#rect = pygame.draw.rect(screen, (black), (175, 75, 200, 100), 2)
-
-			screen.fill(color)
-
-			screen.blit(font.render(FNR_print, True, (255,0,0)), (200, 40))
-			pygame.display.update()
-
-		elif buttonPressed == 'X' or buttonPressed == 'Y':
-			old_FNR = FNR
-			FNR = 1
-			FNR_print = "1"
-			#rect = pygame.draw.rect(screen, (black), (175, 75, 200, 100), 2)
-			screen.fill(color)
-
-			screen.blit(font.render(FNR_print, True, (255,0,0)), (200, 40))
-			pygame.display.update()
-
-
-		#convert throttle to b/w 0 & 40, steering from 0 to 65535
-		# 0 = no throttle, 100 = full throttle; 0 = all the way left, 90 = all the way right
-		if not FNR == old_FNR:
-			old_FNR = FNR
-			pubFNR.publish(FNR)
-
-		publishedSteering = int(0.5*max_steering + steering)
-		if not abs(publishedSteering - old_publishedSteering) < 1000:
-			pubSteering.publish(publishedSteering)
-			old_publishedSteering = publishedSteering
 
 		publishedThrottle = int(throttle)
 		if not publishedThrottle == old_publishedThrottle:
 			pubThrottle.publish(publishedThrottle)
 			old_publishedThrottle = publishedThrottle
+
+		if not FNR == old_FNR:
+			old_FNR = FNR
+			pubFNR.publish(FNR)
+
+		publishedSteering = int(0.5*max_steering + steering)
+		if abs(publishedSteering - old_publishedSteering) > 1000:
+			pubSteering.publish(publishedSteering)
+			old_publishedSteering = publishedSteering
+
 		
-		#screen.blit(font.render(FNR_print, True, (255,0,0)), (200, 40))
-		#screen.blit(font.render(throttle_print, True, (255,0,0)), (200, 70))
-		#screen.blit(font.render(steering_print, True, (255,0,0)), (200, 100))
-		#pygame.display.update()
-
-
 if __name__  == "__main__":
 	print "FNR => UP, DOWN, RSHIFT"
 	print "Steering => LEFT, RIGHT"
