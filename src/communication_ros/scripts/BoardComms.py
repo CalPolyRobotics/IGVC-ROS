50f94c88b852fb5399362da865e92b618607fbe6
willle3456 <willle3456@gmail.com>
2016-04-29 20:57:42 -0700

    temporary BoardComms changes

--- src/communication_ros/scripts/BoardComms.py
+++ src/communication_ros/scripts/BoardComms.py
@@ -3,23 +3,52 @@
 import rospy
 import serial
 import sys
+import Queue
+from crc8 import *
+from config import *
 
 from std_msgs.msg import UInt8, UInt16
 
 
-start_byte_1 = 0xF0
-start_byte_2 = 0x5A
-#sequence number counts the messages up to 255, wrap around to 0
-s_number = 0 #dictionary {message_type: fcn}
-#tracks the message types
-message_type = [i for i in range(0x00,0x17)]
-send_message_type = 0
-#error checking, to be implemented later
-CRC8 = 0
-header_packet_length = 6
-data_msg = 0
+queue = Queue.Queue(maxsize=0)
+seq = 0
+init_crc8()
+
+"""
+@params: num = message type, seq = sequence number, data = data assoicated with the message
+@return: a serial message for that type
+"""
+def create_msg(num, data):
+    global seq
+	start_1 = 0xF0
+	start_2 = 0x5A
+	h_packet = 6
+	CRC_8 = 0
+	msg = bytearray([start_1, start_2, CRC_8, DATA_CODES[DATA_TYPE[num]][0], seq, DATA_CODES[DATA_TYPE[num]][1] + h_packet, data])
+
+    CRC_8 = crc8(1, msg)
+	msg[2] = CRC_8
+
+    return msg
+
+
+"""
+after reading in a serial message, update the data for tracked values
+"""
+def update_values(msg):
+	msg_type = msg[2]
+	DATA_VALUE[DATE_TYPE[msg_type]] = msg_data = msg[len(msg) -1]
+
+def read_msg(serial):
+	tmp = serial.read(6)
+	msg_type = tmp[3]
+	if (tmp is not null) and (tmp[0] == 0xF0) and (tmp[1] == 0x5A):
+		data = serial.read(DATA_CODE[DATA_TYPE[msg_type]][1])
+		tmp.add(data)
+	return tmp
 
 def callbackSFNR(data):
+	global queue
 	rospy.loginfo(rospy.get_name() + "The FNR data is %d ", data.data)
 	global s_number
 	global message_type
@@ -75,12 +104,44 @@ def callbackSSteering(data):
 	packet = bytearray([start_byte_1, start_byte_2, CRC8, send_message_type, s_number, packet_length, (data_msg >> 8) & 0xFF, data_msg & 0xFF])
 	print(''.join(format(x, '02x') for x in packet))
 	ser.write(packet)
+	packet = create_msg(0x06, data)
+	print(''.join(format(x, '02x') for x in packet))
+	queue.put(packet)
+
+def callbackSThrottle(data):
+	global queue
+	packet = create_msg(0x08, data)
+	print(''.join(format(x, '02x') for x in packet))
+	queue.put(packet)
+
+def callbackSSteering(data):
+	global queue
+	packet = create_msg(0x10, data)
+	print(''.join(format(x, '02x') for x in packet))
+	queue.put(packet)
+
+def callbackSSpeed(data):
+	global queue
+	packet = create_msg(0x0A, data)
+	print(''.join(format(x, '02x') for x in packet))
+	queue.put(packet)
+
+def callbackSLights(data):
+	global queue
+	packet = create_msg(0x18, data)
+	print(''.join(format(x, '02x') for x in packet))
+	queue.put(packet)
 
 def listener():
 	rospy.init_node('BoardComms', anonymous=True)
-	rospy.Subscriber('Set_FNR', UInt8, callbackSFNR)
-	rospy.Subscriber('Set_Throttle', UInt16, callbackSThrottle)
-	rospy.Subscriber('Set_Steering', UInt16, callbackSSteering)
+
+    pubData = rospy.Publisher("data", UInt8, queue_size=1000)
+
+	rospy.Subscriber('Set_FNR', UInt8, callbackSFNR())
+	rospy.Subscriber('Set_Throttle', UInt16, callbackSThrottle())
+	rospy.Subscriber('Set_Steering', UInt16, callbackSSteering())
+	rospy.Subscriber('Set_Speed', UInt16, callbackSSpeed())
+	rospy.Subscriber('Set_Lights', UInt8, callbackSLights())
 	#will have other subscribers
 	rospy.spin()
 
@@ -92,4 +153,15 @@ if __name__ == '__main__':
    else:
       ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0)
    listener()
+
+	while rospy_is.shutdown():
+		tmp_msg = read_msg(ser):
+		listener()
+		queue.put(create_msg(data[i], none))
+		if seq > 255:
+			seq = 0
+		else:
+			seq+=1
+        ser.write(queue.get())
+        i+= 1
 
