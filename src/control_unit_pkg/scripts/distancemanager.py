2f06e27853d663847377f8cbbca9e9b0f4130aca
ssmarok <ssmarok@gmail.com>
2016-10-29 20:04:51 -0700

    Cleaned up Milestone 1 code/ added comments (distancemanager.py)

--- src/control_unit_pkg/scripts/distancemanager.py
+++ src/control_unit_pkg/scripts/distancemanager.py
@@ -2,23 +2,31 @@
 import rospy, math
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import UInt16MultiArray, UInt8, UInt16
+#---------------------------------------------------------------------------
+#                                Milestone 1 
+#---------------------------------------------------------------------------
+# This piece of code detects people/objects in the front of the LIDAR  and 
+# changes between Forward, Neutral, Reverse depending on the distance between 
+# the LIDAR and person/object.
+#---------------------------------------------------------------------------
+
+# INITIALIZE GLOBALS
+
 # Half the Width of the Golf Cart
 HALF_WIDTH = .546
-# Declare Publishers
 
+initFNR = True          # Initialize FNR to forward
+ourDistance = 0         # Holds mimimum distance value from findOurDistance callback 
+ourVelocity= 0          # Holds velocity from findOurVelocity callback
+FNR_State = 1           # Should get the FNR State later
+ranges = []             # Holds lidar_scan ranges
 
-#Changing
-initFNR = True
-ourDistance = 0
-ourVelocity= 0
-#Should get the FNR State later
-#FNR_State = 1
-FNR_State = 1
-ranges = []
-#static
-x0 = 2.0
-x1 = 5.0
-minVelocity = 50 #cm/s
+# Static distance and velocity threshold variables
+x0 = 2.0                # Inner Threshold distance (meters)
+x1 = 5.0                # Outer Threshold distance (meters)
+minVelocity = 50        # minimum velocity in  cm/s
+#--------------------------------------------------------------------------
+# range_Rectangle checks to see if lidar value is in front of golf cart
 def range_Rectangle(index, length):
     #TODO: Change values to laser_scan dependent values
     if index > 541/2:
@@ -28,106 +36,93 @@ def range_Rectangle(index, length):
         if length > (HALF_WIDTH/math.cos(math.radians((index - 90)/2))):
             return False
     return True
-
+#---------------------------------------------------------------------------
+# Callback to update the minimumDistance to the smallest distance from lidar range
 def findOurDistance(data):
     global ranges
     global ourDistance
-    ranges = data.data;
-    minimumDistance = 20.0
-    for i in range(130, 421): # Range of lidar data we want to reed
+    ranges = data.data
+    minimumDistance = 20.0                       # Maximum range of lidar
+    for i in range(130, 421):                    # Range of lidar data we want to read (values in front of lidar)
         if range_Rectangle(i, ranges[i]):
-            #print(ranges[i])
-            if (ranges[i] < minimumDistance) and (ranges[i] > 0.1):
-                minimumDistance = ranges[i]
+            if (ranges[i] < minimumDistance) and (ranges[i] > 0.1): 
+            # .1 is threshold distance (distance >20m is given as value close to 0)
+                minimumDistance = ranges[i]      # Update minimumDistance value
             else:
-                #print("Something was in the rectangle but far away")
                 pass
-            #print "In Range Rectangle"
     ourDistance = minimumDistance
-    #print(minimumDistance)
-    #print("The distance calculated to nearest: " , ourDistance)
 
-#Is the cart moving? How fast?
+# Callback to update ourVelocity value
 def findOurVelocity(data):
     global ourVelocity
     try:
         ourVelocity = (data.data[0])/10
     except:
         pass
-        #print("Error: data.data broke")
-        #print(data.data)
-    #print(ourVelocity)
-    #print"SPEED is : ", ourVelocity
 
-def stateMachine():
-    global ourVelocity
-    #print("Our velocity: " , ourVelocity)
-    global FNR_State
-    #print("Our FNR State: " , FNR_State)
-    global ourDistance
-    #print("Our Distance: " , ourDistance)
-    #global initFNR
-    temp = FNR_State
-    #State Machine Logic
-    if FNR_State == 1:
-        #print("we are in state 1")
-        if ((ourDistance < x0) and (ourVelocity < minVelocity)):
-            print("We are changing to state 1.")
-            print("Our distance to change was: " , ourDistance)
-            FNR_State = 0
-        elif ((ourDistance < x0) and (ourVelocity >= minVelocity)):
-            print("We are changing to state 2.")
-            print("Our distance to change was: " , ourDistance)
-            FNR_State = 2
-    elif FNR_State == 0:
-        #print("We are in state 0.")
-        if (ourDistance > x1):
-            print("We are changing to state 1.");
-            print("Our distance to change was: " , ourDistance)
-            FNR_State = 1
-    elif FNR_State == 2:
-        #print("We are in state 2.")
-        if (ourDistance > x0):
-            print("we are changing to state 0")
-            print("Our distance to change was: " , ourDistance)
-            FNR_State = 0
-
-    #Publish FNR State if it changed
-    if FNR_State != temp:
-        publishState(FNR_State)
-    """
-    if initFNR == True:
-        initFNR = False
-        FNR_State = 1
-        publishState(FNR_State)
-    """
-
-def publishState(new_state):
-   PUB_FNR.publish(new_state)
-   FNR_State = new_state
-
-def listener():
-    print("Hello")
-    #rospy.init_node('proximity_manager', anonymous=True)
-    rospy.Subscriber("lidar_scan_ranges", Float32MultiArray, findOurDistance)
-    rospy.Subscriber("Get_Speed", UInt16MultiArray, findOurVelocity)
-    rospy.Subscriber("Get_FNR", UInt8, callbackFNR)
-    # spin() simply keeps python from exiting until this node is stopped
-    while not rospy.is_shutdown():
-        stateMachine() #TODO: Rename StateMachine
-    rospy.spin()
-
+# Callback to update FNR state
 def callbackFNR(data):
     global FNR_state
     FNR_state = data
-    print ("Current State is: %s",  data)
+    print ("Current FNR State is: %s",  data)
+#--------------------------------------------------------------------------
+# Main State Machine for F,N,R state transitions
+def stateMachine():
+    global ourVelocity
+    global FNR_State
+    global ourDistance
+    #global initFNR
+    temp = FNR_State        # To compare initial to final FNR_State
 
+    # State Machine Logic
+    if FNR_State == 1:      # Forward State
+        if ((ourDistance < x0) and (ourVelocity < minVelocity)):
+            print("We are changing to state 0 (Neutral).")
+            print("Our distance to change was: " , ourDistance)
+            FNR_State = 0
+        elif ((ourDistance < x0) and (ourVelocity >= minVelocity)):
+            print("We are changing to state 2 (Reverse).")
+            print("Our distance to change was: " , ourDistance)
+            FNR_State = 2
+    elif FNR_State == 0:    # Neutral State
+        if (ourDistance > x1):
+            print("We are changing to state 1 (Forward).");
+            print("Our distance to change was: " , ourDistance)
+            FNR_State = 1
+    elif FNR_State == 2:    # Reverse State
+        if (ourDistance > x0):
+            print("we are changing to state 0 (Neutral).")
+            print("Our distance to change was: " , ourDistance)
+            FNR_State = 0
+
+    # Publish FNR State if it changed
+    if FNR_State != temp:
+        publishState(FNR_State)
+
+# Publishes new state
+def publishState(new_state):
+   PUB_FNR.publish(new_state)
+   FNR_State = new_state
+#---------------------------------------------------------------------------
+# Subscribers
+def listener():
+    print("Hello! ~ Milestone 1")
+    rospy.Subscriber("lidar_scan_ranges", Float32MultiArray, findOurDistance)
+    rospy.Subscriber("Get_Speed", UInt16MultiArray, findOurVelocity)
+    rospy.Subscriber("Get_FNR", UInt8, callbackFNR)
+    while not rospy.is_shutdown():
+        stateMachine() #TODO: Rename StateMachine
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+#---------------------------------------------------------------------------
 if __name__ == '__main__':
+    # Initialize publisher and node
     PUB_FNR = rospy.Publisher('Set_FNR', UInt8, queue_size=10)
     rospy.init_node('distance_manager', anonymous=True)
+    # Change initial FNR state to Forward
     print("Initialzation: Changing FNR state to 1.")
-    PUB_FNR.publish(1)
+    PUB_FNR.publish(1)  # Forward State
     print("Initialzation: Changed FNR state to 1.")
-
-    #PUB_FNR.publish(1)
+    # Go to main function of node
     listener()
