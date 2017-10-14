d180ba50b12a22b5311927e2c3db47db26d20987
Kyle Wuerch <kyle98678@gmail.com>
2017-10-14 16:09:53 -0700

    Begun writing tests for publishers

--- src/communication_ros/scripts/TestBoardComms.py
+++ src/communication_ros/scripts/TestBoardComms.py
@@ -5,41 +5,61 @@ import binascii
 
 from std_msgs.msg import UInt8, UInt8MultiArray, UInt16MultiArray, UInt16  
 
+"""
 fnr = rospy.Publisher('Set_FNR', UInt8, queue_size=10)
 thrt = rospy.Publisher('Set_Throttle', UInt16, queue_size=10)
 ster = rospy.Publisher('Set_Steering', UInt16, queue_size=10)
 sped = rospy.Publisher('Set_Speed', UInt16, queue_size=10)
 ligt = rospy.Publisher('Set_Lights', UInt16, queue_size=10)
 stop = rospy.Publisher('Stop', UInt8, queue_size=10)
+"""
 
 def initTest():
-   rospy.Subscriber('Status', UInt8, callbackStatus)
-   #rospy.Subscriber('Get_Sonar_1', UInt8, callbackSonar1)
-   #rospy.Subscriber('Get_Sonar_All', Uint8MultiArray, callbackSonarAll)
-   rospy.Subscriber('Get_FNR', UInt8, callbackFNR)
-   rospy.Subscriber('Get_Speed', UInt16MultiArray, callbackSpeed)
-   rospy.Subscriber('Get_Steering', UInt16, callbackSteering)
-   rospy.Subscriber('Get_Battery', UInt16, callabackBattery)
-   rospy.Subscriber('Get_Power', UInt16MultiArray, callbackPower)
+   rospy.Subscriber('Status', UInt8, assertStatus )
+   rospy.Subscriber('Get_Sonar_1', UInt8, assertSon1 )
+   rospy.Subscriber('Get_Sonar_All', Uint8MultiArray, assertSonA )
+   rospy.Subscriber('Get_FNR', UInt8, assertFNR )
+   rospy.Subscriber('Get_Speed', UInt16MultiArray, assertSpeed )
+   rospy.Subscriber('Get_Steering', UInt16, assertSteer )
+   rospy.Subscriber('Get_Battery', UInt16, asserBatt )
+   rospy.Subscriber('Get_Power', UInt16MultiArray, assertPower )
+
+
+getStatPass = "No Response"
+getSon1Pass = "No Response"
+getSonAPass = "No Response"
+getFNRPass = "No Response"
+getSpeedPass = "No Response"
+getSteerPass = "No Response"
+getPowerPass = "No Response"
+getBattPass = "No Response"
+
+def testBoardComms():
+   initTests()
+   
+   time.sleep(5000)
+
+   print( "Status Test: %s", getStatPass )
+   
 
 
 "----------- Callbacks ----------"
 
-def callbackRStatus(data):
+def assertStatus(data):
 
-def callbackRSonar1(data):
+def assertSon1(data):
 
-def callbackRSonarAll(data):
+def assertSonA(data):
 
-def callbackRFNR(data):
+def assertFNR(data):
 
-def callbackRSpeed(data):
+def assertSpeed(data):
 
-def callbackRSteering(data):
+def assertSteer(data):
 
-def callbackRPower(data):
+def assertBatt(data):
 
-def callbackRBattery(data):
+def assertPower(data):
 
 "--------- End Callbacks --------"
 
@@ -52,20 +72,3 @@ publisher_callbacks = {
   0x12 : callbackRSteering,
   0x16 : callbackRBattery,
   0x18 : callbackRPower}
-
-def testBoardComms():
-
-def testSetFNR():
-def testSetSpeed():
-def testSetSteering():
-def testSetThrottle():
-def testSetSpeed():
-def testSetLights():
-def testSetStop():
-
-def testGetFNR():
-def testGetStatus():
-def testGetSpeed():
-def testGetSteering():
-def testGetBattery():
-def testGetPower():
