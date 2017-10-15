bd0e3a13bcf066b65a83e6684eb642abe4cfa188
Sean Wallace <seanswally@gmail.com>
2017-10-14 17:54:19 -0700

    FNR, Speed, Steering tests

--- src/communication_ros/scripts/TestBoardComms.py
+++ src/communication_ros/scripts/TestBoardComms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-import binascii
+import time
 
 from std_msgs.msg import UInt8, UInt8MultiArray, UInt16MultiArray, UInt16  
 
@@ -17,58 +17,86 @@ stop = rospy.Publisher('Stop', UInt8, queue_size=10)
 def initTest():
    rospy.Subscriber('Status', UInt8, assertStatus )
    rospy.Subscriber('Get_Sonar_1', UInt8, assertSon1 )
-   rospy.Subscriber('Get_Sonar_All', Uint8MultiArray, assertSonA )
+   rospy.Subscriber('Get_Sonar_All', UInt8MultiArray, assertSonA )
    rospy.Subscriber('Get_FNR', UInt8, assertFNR )
    rospy.Subscriber('Get_Speed', UInt16MultiArray, assertSpeed )
    rospy.Subscriber('Get_Steering', UInt16, assertSteer )
-   rospy.Subscriber('Get_Battery', UInt16, asserBatt )
+   rospy.Subscriber('Get_Battery', UInt16, assertBatt )
    rospy.Subscriber('Get_Power', UInt16MultiArray, assertPower )
 
+defaultResponse = "\033[1;31mNo Response\033[0;0m"
 
-getStatPass = "No Response"
-getSon1Pass = "No Response"
-getSonAPass = "No Response"
-getFNRPass = "No Response"
-getSpeedPass = "No Response"
-getSteerPass = "No Response"
-getPowerPass = "No Response"
-getBattPass = "No Response"
+getStatPass = defaultResponse
+getSon1Pass = defaultResponse
+getSonAPass = defaultResponse
+getFNRPass = defaultResponse
+getSpeedPass = defaultResponse
+getSteerPass = defaultResponse
+getPowerPass = defaultResponse
+getBattPass = defaultResponse
+
+def printSuccess(msg):
+   print "\033[0;32m" + msg + "\033[0;0m"
 
 def testBoardComms():
-   initTests()
+   initTest()
    
-   time.sleep(5000)
+   printSuccess("Testing Getters...")
+   time.sleep(1)
 
-   print( "Status Test: %s", getStatPass )
-   
+   print "Status Test: ", getStatPass
+   print "Sonar 1 Test: ", getSon1Pass
+   print "Sonar All Test: ", getSonAPass
+   print "FNR Test: ", getFNRPass
+   print "Speed Test: ", getSpeedPass
+   print "Steering Test: ", getSteerPass
+   print "Power Test: ", getPowerPass
+   print "Battery Test: ", getBattPass
 
-
-"----------- Callbacks ----------"
-
+"----------- Callbacks ----------" 
 def assertStatus(data):
+   global getStatPass
+   getStatPass = "Not Implemented"
 
 def assertSon1(data):
+   global getSon1Pass
+   getSon1Pass = "Not Implemented"
 
 def assertSonA(data):
+   global getSonAPass
+   getSonAPass = "Not Implemented"
 
 def assertFNR(data):
+   global getFNRPass
+   # Validate the data is in range[0, 1]
+   if (data.data < 0 or data.data > 2):
+      getFNRPass = "FNR Data is out of range [0,2]: " + data.data
+   else:
+      getFNRPass = "Success"
 
 def assertSpeed(data):
+   global getSpeedPass
+   # Validate speed range [0, 64]
+   if (data.data < 0 or data.data > 64):
+      getSpeedPass = "Speed Data is out of range [0,64]:"
+   else:
+      getSpeedPass = "Success"
 
 def assertSteer(data):
+   global getSteerPass
+   # Validate steering range [0, 65535]
+   if (data.data < 0 or data.data > 64):
+      getSteerPass = "Steer Data is out of range [0,65535]:"
+   else:
+      getSteerPass = "Success"
 
 def assertBatt(data):
+   global getStatPass
 
 def assertPower(data):
+   global getStatPass
 
 "--------- End Callbacks --------"
 
-publisher_callbacks = {
-  0x00 : callbackRStatus,
-  0x02 : callbackRSonar1,
-  0x04 : callbackRSonarAll,
-  0x08 : callbackRFNR,
-  0x0E : callbackRSpeed,
-  0x12 : callbackRSteering,
-  0x16 : callbackRBattery,
-  0x18 : callbackRPower}
+if __name__ == '__main__':
+   testBoardComms()
