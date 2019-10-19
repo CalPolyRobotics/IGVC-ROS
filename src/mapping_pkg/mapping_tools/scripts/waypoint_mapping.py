4461a952ad914f3208a98ac718d197f4803bf399
Tristan <tcavarno@gmail.com>
2019-10-19 13:36:23 -0700

    Added a launch file for the waypoint tracker and made style changes to the waypoint mapper script

--- src/mapping_pkg/mapping_tools/scripts/waypoint_mapping.py
+++ src/mapping_pkg/mapping_tools/scripts/waypoint_mapping.py
@@ -5,30 +5,38 @@ import csv
 from sensor_msgs.msg import NavSatFix
 
 class WaypointMapper:
+    '''
+    To save waypoints (Latitude , Longitude) of a path to a csv file.
+    '''
     def __init__(self, file_name):
         self.prev_gps_info = None
         
-        rospy.init_node('waypoint_mapper', anonymous=True)
-        rospy.Subscriber('fix', NavSatFix, self.waypoint_cb)
-        
         self.csvfile = open(file_name, "w")
         self.writer = csv.writer(self.csvfile , delimiter = ",")
-        self.writer.writerow(("Latitude" , "Longitude"))
-
+        self.writer.writerow(("Latitude" , "Longitude"))       
+        
+        rospy.init_node('waypoint_mapper', anonymous=True)
+        rospy.Subscriber('fix', NavSatFix, self.waypoint_cb)
+    
     def waypoint_cb(self , gps_info):
+       '''
+       Call back for gps information
+       
+       Args:
+            gps_info: a NavSatFix message
+       '''
         lat_long = (gps_info.latitude, gps_info.longitude)
         
-        if self.prev_gps_info is None or geopy.distance.distance(self.prev_gps_info , lat_long) >= 3 :
-            prev_gps_info = (gps_info.latitude , gps_info.longitude)
+        # records the waypoint if the distance from the previously recorded waypoint is greater than 3 meters
+        if self.prev_gps_info is None or geopy.distance.distance(self.prev_gps_info , lat_long) >= 3:
+            prev_gps_info = (gps_info.latitude , gps_info.longitude) # update the previous gps info to the last recorded waypoint
             rospy.loginfo(prev_gps_info)
             self.writer.writerow(prev_gps_info)
 
 if __name__ == "__main__":
-    
     try:
         file_name = rospy.get_param("waypoint_map_file", "Waypoint_map_1.csv")
         waypoint_mapper = WaypointMapper(file_name)
-        
     except rospy.ROSInterruptException:
         waypoint_mapper.csvfile.close()
     rospy.spin()
